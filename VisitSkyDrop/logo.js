5bfe5e759fe3eb0385dce4f326e8f8410e7fece2
Filip Gogic <filip.gogic@visit.com>
2023-03-03 10:39:31 +0100

    balko on fire

--- VisitSkyDrop/logo.js
+++ VisitSkyDrop/logo.js
@@ -13,13 +13,13 @@ class Logo {
         this.y = y;
         this.r = 16
         switch(logoNumber){
-            case 1: {this.picture = loadImage(airbnb), this.points = -100, this.picHeight = 100, this.picWidth = 100}break;
-            case 2: {this.picture = loadImage(bookvisit), this.points = 100, this.picHeight = 100, this.picWidth = 100}break;
-            case 3: {this.picture = loadImage(booking), this.points = -200, this.picHeight = 100, this.picWidth = 100}break;
-            case 4: {this.picture = loadImage(citybreak), this.points = 100, this.picHeight = 70, this.picWidth = 100}break;
-            case 5: {this.picture = loadImage(iticket), this.points = 100, this.picHeight = 50, this.picWidth = 100}break;
-            case 6: {this.picture = loadImage(visit), this.points = 300, this.picHeight = 100, this.picWidth = 100}break;
-            case 7: {this.picture = loadImage(balko), this.points = Math.round(random(-1000,1000)), this.picHeight = 125, this.picWidth = 125}break;
+            case 1: {this.picture = loadImage(airbnb), this.points = -100, this.picHeight = 100, this.picWidth = 100, this.id = logoNumber}break;
+            case 2: {this.picture = loadImage(bookvisit), this.points = 100, this.picHeight = 100, this.picWidth = 100, this.id = logoNumber}break;
+            case 3: {this.picture = loadImage(booking), this.points = -200, this.picHeight = 100, this.picWidth = 100, this.id = logoNumber}break;
+            case 4: {this.picture = loadImage(citybreak), this.points = 100, this.picHeight = 70, this.picWidth = 100, this.id = logoNumber}break;
+            case 5: {this.picture = loadImage(iticket), this.points = 100, this.picHeight = 50, this.picWidth = 100, this.id = logoNumber}break;
+            case 6: {this.picture = loadImage(visit), this.points = 300, this.picHeight = 100, this.picWidth = 100, this.id = logoNumber}break;
+            case 7: {this.picture = loadImage(balko), this.points = Math.round(random(-1000,1000)), this.picHeight = 125, this.picWidth = 125, this.id = logoNumber}break;
             default: return;
         }
     }
@@ -30,6 +30,14 @@ class Logo {
         fill(0,0,0,0);
         image(this.picture, this.x, this.y, this.picWidth, this.picHeight);
         noFill();
+        if(this.id == 7){
+            fill('RED');
+            textSize(20);
+            textAlign(CENTER,CENTER);
+            textStyle(BOLD);
+            text('I\'M ON FIRE!', this.x, this.y-75);
+            noFill();
+        }
 
     }
 
